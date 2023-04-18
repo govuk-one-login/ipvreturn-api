0ee6a0d8e7192f8d19fdad9fea25e7f5332fc9ce
Ayesha Khan <ayesha.khan@digital.cabinet-office.gov.uk>
2023-04-18 15:46:56 +0100

    Adjusted infra test to reflect singular kms key

--- infra-l2-kms/src/tests/infra/infra.test.ts
+++ infra-l2-kms/src/tests/infra/infra.test.ts
@@ -12,8 +12,8 @@ beforeAll(() => {
   template = Template.fromJSON(yamlTemplate)
 })
 
-it("should define 2 KMS keys", async () => {
-  template.resourceCountIs('AWS::KMS::Key', 2)
+it("should define 1 KMS keys", async () => {
+  template.resourceCountIs('AWS::KMS::Key', 1)
 })
 
 it("each KMS key should have an alias", async () => {
