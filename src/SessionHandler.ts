6ec8a660659a197641806c17152165ebe7ba0f66
Richa Misra <richa.misra@digital.cabinet-office.gov.uk>
2023-05-15 14:42:59 +0100

    Merge from main

--- src/SessionHandler.ts
+++ src/SessionHandler.ts
@@ -6,6 +6,7 @@ import { ResourcesEnum } from "./models/enums/ResourcesEnum";
 // import { AppError } from "./utils/AppError";
 import { HttpCodesEnum } from "./utils/HttpCodesEnum";
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
+import {SessionEventProcessor} from "./services/SessionEventProcessor";
 
 const POWERTOOLS_METRICS_NAMESPACE = process.env.POWERTOOLS_METRICS_NAMESPACE ? process.env.POWERTOOLS_METRICS_NAMESPACE : "CIC-CRI";
 const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : "DEBUG";
@@ -22,26 +23,23 @@ class Session implements LambdaInterface {
   @metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
   @logger.injectLambdaContext()
   async handler(event: APIGatewayProxyEvent, _context: any): Promise<APIGatewayProxyResult> {
+    logger.debug("metrics is", { metrics });
+    logger.debug("Event received", { event });
+
     switch (event.resource) {
       case ResourcesEnum.SESSION:
-        try {
-          logger.debug("metrics is", { metrics });
-          logger.debug("Event received", { event });
-
-          // add code here
-
-          return new Response(HttpCodesEnum.OK, "OK");
-
-        } catch (err: any) {
-          logger.error("An error has occurred.", { err });
-          // if (err instanceof AppError) {
-          //   return new Response(err.statusCode, err.message);
-          // }
-          return new Response(HttpCodesEnum.SERVER_ERROR, "An error has occurred");
+        if (event.httpMethod === "POST") {
+          try {
+            logger.info("Got Session request:", {event});
+            return await SessionEventProcessor.getInstance(logger, metrics).processRequest(event);
+          } catch (err) {
+            logger.error({message: "An error has occurred. ", err});
+            return new Response(HttpCodesEnum.SERVER_ERROR, "An error has occurred");
+          }
         }
+        return new Response(HttpCodesEnum.NOT_FOUND, "");
       default:
-      //   throw new AppError("Requested resource does not exist" + { resource: event.resource }, HttpCodesEnum.NOT_FOUND);
-      return new Response(HttpCodesEnum.NOT_FOUND, "Resource not found");
+        return new Response(HttpCodesEnum.NOT_FOUND, "Resource not found");
     }
   }
 
