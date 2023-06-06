f6e5ae20413d0f5ec7a0fbf2fc10c8944ee81e02
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-06-06 15:40:30 +0100

    Removing the commented lines and unwanted files.

--- src/services/GovNotifyErrorMapper.ts
+++ src/services/GovNotifyErrorMapper.ts
@@ -1,6 +1,3 @@
-//import { AppCodes } from "../models/AppCodes";
-// @ts-ignore
-import { find } from "lodash";
 import { AppError } from "../utils/AppError";
 import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
 
@@ -8,13 +5,6 @@ import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
  * Class to map error received from Gov Notify service to App Error object
  */
 export class GovNotifyErrorMapper {
-	//
-	// private static readonly errorCodeMap = new Map<number, AppCodes[]>([
-	// 	[ 400, [ AppCodes.E7408, AppCodes.E7409 ] ],
-	// 	[ 403, [ AppCodes.E8306, AppCodes.E5210] ],
-	// 	[ 429, [ AppCodes.W4101, AppCodes.E6101] ],
-	// 	[ 500, [ AppCodes.E5202] ],
-	// ]);
 
 	/**
 	 * Method that takes an error raised by SendEmailService and translates the error into an AppError with lambda specific AppCode
@@ -23,7 +13,6 @@ export class GovNotifyErrorMapper {
 	 */
 	map(_statusCode: number, errMessage: string): AppError {
     	const statusCode: number = _statusCode || HttpCodesEnum.SERVER_ERROR;
-    	const message: string    = errMessage || "Code error";
     	const shouldRetry = (_statusCode === 500 || _statusCode === 429) ? true : false;
     	return new AppError(statusCode, errMessage, { shouldRetry });
 	}
