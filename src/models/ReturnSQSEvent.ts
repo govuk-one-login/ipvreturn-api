5ff86398942efdfb610138a63a8881f3ca4c4662
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-08-18 13:13:00 +0100

    F2F-1090 test updates

--- src/models/ReturnSQSEvent.ts
+++ src/models/ReturnSQSEvent.ts
@@ -14,7 +14,6 @@ export interface ReturnSQSEvent {
 		govuk_signin_journey_id?: string;
 		user_id: string;
 		email?: string;
-		govuk_signin_journey_id: string;
 	};
 	restricted?: {
 		nameParts: NamePart[];
