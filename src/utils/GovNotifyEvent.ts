6e1d65faa6e10834c495ce17f7147e9526902020
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-09-07 23:13:25 +0100

    WIP- Draft code to validate the sessionEvent record fields and create newEmail SQS message and fallback to oldEmail message if some fields are missing.

--- src/utils/GovNotifyEvent.ts
+++ src/utils/GovNotifyEvent.ts
@@ -1,3 +1,5 @@
+import { PostOfficeInfo, PostOfficeVisitDetails } from "../models/SessionReturnRecord";
+
 export interface GovNotifyEvent {
 	"Message": {
 		"userId": string;
@@ -5,18 +7,43 @@ export interface GovNotifyEvent {
 		"firstName": string;
 		"lastName": string;
 		"messageType": string;
+		"documentType"?: string;
+		"documentExpiryDate"?: string;
+		"poAddress"?: string;
+		"poVisitDate"?: string;
+		"poVisitTime"?: string;
 	};
 }
 
-export const buildGovNotifyEventFields = (userId: string, email: string, firstName: string, lastName: string): GovNotifyEvent => {
-
+export const buildOldGovNotifyEventFields = (userId: string, email: string, firstName: string, lastName: string): GovNotifyEvent => {
+	
 	return {
 		Message : {
 			userId,
 			emailAddress: email,
 			firstName,
 			lastName,
-			messageType: "email",
+			messageType: "oldEmail",
 		},
-	};
+	};		
+	
+};
+
+export const buildNewGovNotifyEventFields = (userId: string, email: string, firstName: string, lastName: string, documentType: string , documentExpiryDate: string, poInfo: PostOfficeInfo , poVisitDetails: PostOfficeVisitDetails ): GovNotifyEvent => {
+	
+	return {
+		Message : {
+			userId,
+			emailAddress: email,
+			firstName,
+			lastName,
+			documentType,
+			poAddress: poInfo.address + " " + poInfo.post_code,
+			poVisitDate: poVisitDetails.post_office_date_of_visit,
+			poVisitTime: poVisitDetails.post_office_time_of_visit,
+			documentExpiryDate,
+			messageType: "newEmail",
+		},
+	};	
+
 };
