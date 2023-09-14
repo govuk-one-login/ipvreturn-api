aa33ea20ddd0808120a0c1192805e373867880bf
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2023-09-14 10:57:31 +0100

    add env variables needed for govnotify test (#77)

--- run-tests.sh
+++ run-tests.sh
@@ -14,7 +14,7 @@ export API_TEST_SESSION_EVENTS_TABLE=$(remove_quotes $CFN_SessionEventsTable)
 #The CFN variables seem to include quotes when used in tests these must be removed before assigning them.
 export API_TEST_SQS_TXMA_CONSUMER_QUEUE=$(remove_quotes $CFN_MockTxMASQSQueueUrl)
 export DEV_IPR_TEST_HARNESS_URL=$(remove_quotes $CFN_IpvReturnTestHarnessURL)
-
+export GOVUKNOTIFYAPI=$(remove_quotes $CFN_GovNotifyAPIURL)
 
 cd /src; npm run test:api
 error_code=$?
