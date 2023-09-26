1e7c88db5f9d12eb76d034b5a965fa4f93aa3fd5
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-09-26 14:13:05 +0100

    KIKI-1062 Test Updates

--- src/tests/api/postEventStream.test.ts
+++ src/tests/api/postEventStream.test.ts
@@ -1,6 +1,7 @@
 import {
 	VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT,
 	VALID_IPV_F2F_CRI_VC_CONSUMED_TXMA_EVENT,
+	VALID_IPV_F2F_DOCUMENT_UPLOADED_TXMA_EVENT,
 	VALID_F2F_YOTI_START_TXMA_EVENT,
 } from "../data/sqs-events";
 import "dotenv/config";
@@ -20,6 +21,7 @@ describe("post event processor", () => {
 	it("when all 3 events are sent, a Dynamo record with the details of all three events populated", async () => {
     	await postMockEvent(VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT, userId, true);
 		await postMockEvent(VALID_F2F_YOTI_START_TXMA_EVENT, userId, false);
+		await postMockEvent(VALID_IPV_F2F_DOCUMENT_UPLOADED_TXMA_EVENT, userId, false);
 		await postMockEvent(VALID_IPV_F2F_CRI_VC_CONSUMED_TXMA_EVENT, userId, false);
 
 		const response = await getSessionByUserId(userId, constants.API_TEST_SESSION_EVENTS_TABLE!);
@@ -60,5 +62,48 @@ describe("post event processor", () => {
 		expect(response?.clientName).toBe("ekwU");
 		expect(response?.redirectUri).toBe("REDIRECT_URL");
 		expect(response?.userEmail).toBe(constants.API_TEST_EMAIL_ADDRESS);
+		expect(response?.documentType).toBe("PASSPORT");
+		expect(response?.documentExpiryDate).toBe("2030-11-21");
+		expect(response?.postOfficeInfo).toEqual([
+			{
+				"M": {
+					"address": {
+						"S": "1 The Street, Funkytown"
+					},
+					"location": {
+						"L": [
+							{
+								"M": {
+									"latitude": {
+										"N": "0.34322"
+									},
+									"longitude": {
+										"N": "-42.48372"
+									}
+								}
+							}
+						]
+					},
+					"name": {
+						"S": "Post Office Name"
+					},
+					"post_code": {
+						"S": "N1 2AA"
+					}
+				}
+			}
+		]);
+		expect(response?.postOfficeVisitDetails).toEqual([
+			{
+				"M": {
+					"post_office_date_of_visit": {
+						"S": "7 September 2023"
+					},
+					"post_office_time_of_visit": {
+						"S": "4:43 pm"
+					}
+				}
+			}
+		]);
 	}, 10000); // timeout set to 10s to avoid infinite loop
 });
