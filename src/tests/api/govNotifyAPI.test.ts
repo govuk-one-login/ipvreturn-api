79b9dd760256fd6838b5962e93869d214a4dd3ae
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-09-27 10:05:40 +0100

    [KIWI-1105] fixes typo

--- src/tests/api/govNotifyAPI.test.ts
+++ src/tests/api/govNotifyAPI.test.ts
@@ -1,17 +1,15 @@
 import govNotifyRequestData from "../data/govNotifyStubPayload.json";
 import { postGovNotifyRequest } from "./utils/ApiTestSteps";
 
-describe("GovNotify Endpoint /v2/notifications/emai", () => {
-
-	//responseCode
-	const postGovNotifyParams = [
+describe("GovNotify Endpoint /v2/notifications/email", () => {
+	const responseCodes = [
 		[400],
 		[403],
 		[429],
 		[500],
 		[201],
 	];
-	it.each(postGovNotifyParams)("GovNotify - expect '%i' response on POST/v2/notifications/email", async (govNotifyDelimitator) => {
+	it.each(responseCodes)("GovNotify - expect '%i' response on POST/v2/notifications/email", async (govNotifyDelimitator) => {
 		const response = await postGovNotifyRequest(govNotifyDelimitator, govNotifyRequestData);
 
 		console.log("post response: " + JSON.stringify(response.data));
