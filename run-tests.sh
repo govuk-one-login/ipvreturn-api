2d5ad79d90b56f0f4c7c2784e04f25f02c184998
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-09-29 14:35:45 +0100

    [KIWI-1263] Testing

--- run-tests.sh
+++ run-tests.sh
@@ -14,7 +14,13 @@ export GOVUKNOTIFYAPI=$(remove_quotes $CFN_GovNotifyAPIURL)
 export API_TEST_SQS_TXMA_CONSUMER_QUEUE=$(remove_quotes $CFN_MockTxMASQSQueue)
 export API_TEST_GOV_NOTIFY_SQS_QUEUE=$(remove_quotes $CFN_GovNotifySQSQueue)
 export API_TEST_SESSION_EVENTS_TABLE=$(remove_quotes $CFN_SessionEventsTable)
-export DEV_IPR_TEST_HARNESS_URL=$(remove_quotes $CFN_IpvReturnTestHarnessURL)
+export DEV_IPR_TEST_HARNESS_URL="https://ipvreturn-test-harness-ccooling-1-testharness.return.dev.account.gov.uk"
+# export AWS_ACCESS_KEY_ID=$(remove_quotes $CFN_AWS_ACCESS_KEY_ID)
+# export AWS_SECRET_ACCESS_KEY=$(remove_quotes $CFN_$AWS_SECRET_ACCESS_KEY)
+# export AWS_SESSION_TOKEN=$(remove_quotes $CFN_$AWS_SESSION_TOKEN)
+
+aws sts get-caller-identity
+
 
 cd /src; npm run test:api
 error_code=$?
