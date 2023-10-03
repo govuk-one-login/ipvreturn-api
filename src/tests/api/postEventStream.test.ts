26d89f161ba7704c884d6209ff59561d4254a47d
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-10-03 14:28:42 +0100

    QA Updates

--- src/tests/api/postEventStream.test.ts
+++ src/tests/api/postEventStream.test.ts
@@ -1,7 +1,8 @@
 import {
 	VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT,
 	VALID_IPV_F2F_CRI_VC_CONSUMED_TXMA_EVENT,
-	VALID_F2F_YOTI_START_TXMA_EVENT,
+	VALID_F2F_YOTI_START_WITH_PO_DOC_DETAILS_TXMA_EVENT,
+	VALID_F2F_DOCUMENT_UPLOADED_TXMA_EVENT,
 } from "../data/sqs-events";
 import "dotenv/config";
 import { randomUUID } from "crypto";
@@ -18,8 +19,9 @@ describe("post event processor", () => {
 	});
 
 	it("when all 3 events are sent, a Dynamo record with the details of all three events populated", async () => {
-		await postMockEvent(VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT, userId, true);
-		await postMockEvent(VALID_F2F_YOTI_START_TXMA_EVENT, userId, false);
+    	await postMockEvent(VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT, userId, true);
+		await postMockEvent(VALID_F2F_YOTI_START_WITH_PO_DOC_DETAILS_TXMA_EVENT, userId, false);
+		await postMockEvent(VALID_F2F_DOCUMENT_UPLOADED_TXMA_EVENT, userId, false);
 		await postMockEvent(VALID_IPV_F2F_CRI_VC_CONSUMED_TXMA_EVENT, userId, false);
 
 		const response = await getSessionByUserId(userId, constants.API_TEST_SESSION_EVENTS_TABLE!);
