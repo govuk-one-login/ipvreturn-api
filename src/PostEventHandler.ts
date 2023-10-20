7472a6042fd843067b8fc026cc17e020aa015cb3
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-10-20 11:46:38 +0100

    wip

--- src/PostEventHandler.ts
+++ src/PostEventHandler.ts
@@ -15,45 +15,45 @@ const logger = new Logger({
 
 const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE, serviceName: POWERTOOLS_SERVICE_NAME });
 
+const EMPTY_BATCH_RESPONSE: SQSBatchResponse = {
+	batchItemFailures: [],
+};
+
 class PostEventHandler implements LambdaInterface {
-
-	@metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
-	async handler(event: SQSEvent, context: any): Promise<SQSBatchResponse> {
-
-		// clear PersistentLogAttributes set by any previous invocation, and add lambda context for this invocation
+	async handler(event: SQSEvent, context: any): Promise < SQSBatchResponse > {
 		logger.setPersistentLogAttributes({});
 		logger.addContext(context);
 
-		if (event.Records.length === 1) {
-			let body;
-			const record: SQSRecord = event.Records[0];
-
-			try {
-				body = JSON.parse(record.body);
-			} catch (error) {
-				logger.error({ message:"Received invalid JSON in the SQS event record.body" });
-				return { batchItemFailures:[] };
-			}
-
-			logger.debug("Starting to process record", { event_name: body.event_name });
-
-			try {
-				await PostEventProcessor.getInstance(logger, metrics).processRequest(record.body);
-
-				logger.debug("Finished processing record from SQS");
-				return { batchItemFailures:[] };
-
-			} catch (error: any) {
-				logger.error({ message: "SQS Event could not be processed", error });
-				return { batchItemFailures:[] };
-			}
-
-		} else {
+		if (event.Records.length !== 1) {
 			logger.warn({ message: "Unexpected no. of records received", numOfRecords: event.Records.length });
-			return { batchItemFailures:[] };
+			return EMPTY_BATCH_RESPONSE;
+		}
+
+		const record: SQSRecord = event.Records[0];
+		let body;
+
+		try {
+			body = JSON.parse(record.body);
+		} catch (error) {
+			logger.error({
+				message: "Received invalid JSON in the SQS event record.body",
+			});
+			return EMPTY_BATCH_RESPONSE;
+		}
+
+		logger.debug("Starting to process record", {
+			event_name: body.event_name,
+		});
+
+		try {
+			await PostEventProcessor.getInstance(logger, metrics).processRequest(record.body);
+			logger.debug("Finished processing record from SQS");
+			return EMPTY_BATCH_RESPONSE;
+		} catch (error: any) {
+			logger.error({ message: "SQS Event could not be processed", error });
+			return EMPTY_BATCH_RESPONSE;
 		}
 	}
-
 }
 
 const handlerClass = new PostEventHandler();
--- src/services/PostEventProcessor.ts
+++ src/services/PostEventProcessor.ts
@@ -1,6 +1,5 @@
 import { Logger } from "@aws-lambda-powertools/logger";
 import { Metrics } from "@aws-lambda-powertools/metrics";
-
 import { AppError } from "../utils/AppError";
 import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
 import { Constants } from "../utils/Constants";
@@ -8,13 +7,10 @@ import { IPRService } from "./IPRService";
 import { EnvironmentVariables } from "./EnvironmentVariables";
 import { ServicesEnum } from "../models/enums/ServicesEnum";
 import { createDynamoDbClient } from "../utils/DynamoDBFactory";
-import {
-	ReturnSQSEvent,
-} from "../models/ReturnSQSEvent";
+import { ReturnSQSEvent } from "../models/ReturnSQSEvent";
 import { SessionReturnRecord } from "../models/SessionReturnRecord";
 import { absoluteTimeNow } from "../utils/DateTimeUtils";
 import { MessageCodes } from "../models/enums/MessageCodes";
-
 
 export class PostEventProcessor {
 	private static instance: PostEventProcessor;
@@ -41,176 +37,166 @@ export class PostEventProcessor {
 		return PostEventProcessor.instance;
 	}
 
-	async processRequest(eventBody: any): Promise<any> {
-		try {
-			const eventDetails: ReturnSQSEvent = JSON.parse(eventBody);
-			const eventName = eventDetails.event_name;
+  async processRequest(eventBody: any): Promise<any> {
+    try {
+      const eventDetails: ReturnSQSEvent = JSON.parse(eventBody);
+      const eventName = eventDetails.event_name;
+      let updateExpression: string = "";
+      let expressionAttributeValues: { [key: string]: any } = {};
 
-			if (!eventDetails.event_id) {
-				this.logger.error({ message: "Missing event_id in the incoming SQS event" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-				throw new AppError(HttpCodesEnum.SERVER_ERROR, "Missing info in sqs event");
-			}
-			this.logger.appendKeys({ event_id: eventDetails.event_id });
+      if (!eventDetails.event_id) {
+        this.handleError("Missing event_id in the incoming SQS event", MessageCodes.MISSING_MANDATORY_FIELDS);
+      }
 
-			this.logger.info({ message: "Received SQS event with eventName ", eventName });
-			if (!this.checkIfValidString([eventName]) || !eventDetails.timestamp) {
+      this.logger.appendKeys({ event_id: eventDetails.event_id });
 
-				this.logger.error({ message: "Missing or invalid value for any or all of event name, timestamp in the incoming SQS event" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-				throw new AppError(HttpCodesEnum.SERVER_ERROR, "Missing info in sqs event");
-			}
+      this.logger.info({ message: `Received SQS event with eventName ${eventName}` });
 
-			if (!eventDetails.user) {
-				this.logger.error({ message: "Missing user details in the incoming SQS event" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS } );
-				throw new AppError(HttpCodesEnum.SERVER_ERROR, "Missing info in sqs event");
-			}
-			const userDetails = eventDetails.user;
-			this.logger.appendKeys({ govuk_signin_journey_id: userDetails.govuk_signin_journey_id });
+      if (!this.checkIfValidString([eventName]) || !eventDetails.timestamp) {
+        this.handleError("Missing or invalid value for event name or timestamp in the incoming SQS event", MessageCodes.MISSING_MANDATORY_FIELDS);
+      }
 
-			if (!this.checkIfValidString([userDetails.user_id])) {
-				this.logger.error({ message: "Missing or invalid value for userDetails.user_id in event payload" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-				throw new AppError(HttpCodesEnum.SERVER_ERROR, "Missing info in sqs event");
-			}
+      if (!eventDetails.user) {
+        this.handleError("Missing user details in the incoming SQS event", MessageCodes.MISSING_MANDATORY_FIELDS);
+      }
 
-			const userId = userDetails.user_id;
-			// Do not process the event if the event is already processed or flagged for deletion
-			const isFlaggedForDeletionOrEventAlreadyProcessed = await this.iprService.isFlaggedForDeletionOrEventAlreadyProcessed(userId, eventName);
-			if (isFlaggedForDeletionOrEventAlreadyProcessed) {
-				this.logger.info( { message: "Record flagged for deletion or event already processed, skipping update" });
-				return "Record flagged for deletion or event already processed, skipping update";
-			}
+      const userDetails = eventDetails.user;
+      this.logger.appendKeys({ govuk_signin_journey_id: userDetails.govuk_signin_journey_id });
 
-			let updateExpression, expressionAttributeValues: { [key: string]: any }, expiresOn;
+      if (!this.checkIfValidString([userDetails.user_id])) {
+        this.handleError("Missing or invalid value for userDetails.user_id in event payload", MessageCodes.MISSING_MANDATORY_FIELDS);
+      }
 
-			//Set default TTL to 12hrs to expire any records not meant for F2F
-			expiresOn = absoluteTimeNow() + this.environmentVariables.initialSessionReturnRecordTtlSecs();
+      const userId = userDetails.user_id;
 
-			if (eventName === Constants.F2F_YOTI_START) {
-				//Reset TTL to 11days for F2F journey
-				expiresOn = absoluteTimeNow() + this.environmentVariables.sessionReturnRecordTtlSecs();
-			}
+      const isFlaggedForDeletionOrEventAlreadyProcessed = await this.iprService.isFlaggedForDeletionOrEventAlreadyProcessed(userId, eventName);
+      
+      if (isFlaggedForDeletionOrEventAlreadyProcessed) {
+        this.logger.info({ message: "Record flagged for deletion or event already processed, skipping update" });
+        return "Record flagged for deletion or event already processed, skipping update";
+      }
 
-			const returnRecord = new SessionReturnRecord(eventDetails, expiresOn );
-			switch (eventName) {
-				case Constants.AUTH_IPV_AUTHORISATION_REQUESTED: {
-					if (!this.checkIfValidString([userDetails.email, eventDetails.client_id, eventDetails.clientLandingPageUrl])) {
-						this.logger.warn({ message: "Missing or invalid value for any or all of userDetails.email, eventDetails.client_id, eventDetails.clientLandingPageUrl fields required for AUTH_IPV_AUTHORISATION_REQUESTED event type" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-						return `Missing info in sqs ${Constants.AUTH_IPV_AUTHORISATION_REQUESTED} event, it is unlikely that this event was meant for F2F`;
-					}
+      let expiresOn = absoluteTimeNow() + this.environmentVariables.initialSessionReturnRecordTtlSecs();
 
-					updateExpression = "SET ipvStartedOn = :ipvStartedOn, userEmail = :userEmail, clientName = :clientName,  redirectUri = :redirectUri, expiresOn = :expiresOn";
-					expressionAttributeValues = {
-						":userEmail": returnRecord.userEmail,
-						":ipvStartedOn": returnRecord.ipvStartedOn,
-						":clientName": returnRecord.clientName,
-						":redirectUri": returnRecord.redirectUri,
-						":expiresOn": returnRecord.expiresDate,
-					};
-					break;
+      if (eventName === Constants.F2F_YOTI_START) {
+        expiresOn = absoluteTimeNow() + this.environmentVariables.sessionReturnRecordTtlSecs();
+      }
+
+      const returnRecord = new SessionReturnRecord(eventDetails, expiresOn);
+
+      switch (eventName) {
+        case Constants.AUTH_IPV_AUTHORISATION_REQUESTED: {
+          if (!this.checkIfValidString([userDetails.email, eventDetails.client_id, eventDetails.clientLandingPageUrl])) {
+            this.logger.warn({ message: "Missing or invalid value for fields required for AUTH_IPV_AUTHORISATION_REQUESTED event type" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
+            return `Missing info in sqs ${Constants.AUTH_IPV_AUTHORISATION_REQUESTED} event, it is unlikely that this event was meant for F2F`;
+          }
+
+          updateExpression = "SET ipvStartedOn = :ipvStartedOn, userEmail = :userEmail, clientName = :clientName,  redirectUri = :redirectUri, expiresOn = :expiresOn";
+          expressionAttributeValues = {
+            ":userEmail": returnRecord.userEmail,
+            ":ipvStartedOn": returnRecord.ipvStartedOn,
+            ":clientName": returnRecord.clientName,
+            ":redirectUri": returnRecord.redirectUri,
+            ":expiresOn": returnRecord.expiresDate,
+          };
+          break;
+        }
+        case Constants.F2F_YOTI_START: {
+          updateExpression = "SET journeyWentAsyncOn = :journeyWentAsyncOn, expiresOn = :expiresOn";
+          expressionAttributeValues = {
+            ":journeyWentAsyncOn": returnRecord.journeyWentAsyncOn,
+            ":expiresOn": returnRecord.expiresDate,
+          };
+
+          if (returnRecord.postOfficeInfo) {
+            updateExpression += ", postOfficeInfo = :postOfficeInfo";
+            expressionAttributeValues[":postOfficeInfo"] = returnRecord.postOfficeInfo;
+          } else {
+            this.logger.info(`No post_office_details in ${eventName} event`);
+          }
+
+          if (returnRecord.documentType) {
+            updateExpression += ", documentType = :documentType";
+            expressionAttributeValues[":documentType"] = returnRecord.documentType;
+          } else {
+            this.logger.info(`No document_details in ${eventName} event`);
+          }
+
+          if (returnRecord.clientSessionId) {
+            updateExpression += ", clientSessionId = :clientSessionId";
+            expressionAttributeValues[":clientSessionId"] = returnRecord.clientSessionId;
+          } else {
+            this.logger.info(`No govuk_signin_journey_id in ${eventName} event`);
+          }
+          break;
+        }
+        case Constants.IPV_F2F_CRI_VC_CONSUMED: {
+          if (!eventDetails.restricted || !eventDetails.restricted.nameParts) {
+            this.handleError("Missing nameParts fields required for IPV_F2F_CRI_VC_CONSUMED event type", MessageCodes.MISSING_MANDATORY_FIELDS);
+          }
+
+          updateExpression = "SET readyToResumeOn = :readyToResumeOn, nameParts = :nameParts";
+          expressionAttributeValues = {
+            ":readyToResumeOn": returnRecord.readyToResumeOn,
+            ":nameParts": returnRecord.nameParts,
+          };
+
+          if (returnRecord.documentExpiryDate) {
+            updateExpression += ", documentExpiryDate = :documentExpiryDate";
+            expressionAttributeValues[":documentExpiryDate"] = returnRecord.documentExpiryDate;
+          } else {
+            this.logger.info(`No docExpiryDate in ${eventName} event`);
+          }
+          break;
+        }
+        case Constants.F2F_DOCUMENT_UPLOADED: {
+          if (!eventDetails.extensions || !eventDetails.extensions.post_office_visit_details) {
+            this.handleError("Missing post_office_visit_details fields required for F2F_DOCUMENT_UPLOADED event type", MessageCodes.MISSING_MANDATORY_FIELDS);
+          }
+
+          updateExpression = "SET documentUploadedOn = :documentUploadedOn, postOfficeVisitDetails = :postOfficeVisitDetails";
+          expressionAttributeValues = {
+            ":documentUploadedOn": returnRecord.documentUploadedOn,
+            ":postOfficeVisitDetails": returnRecord.postOfficeVisitDetails,
+          };
+          break;
+        }
+        case Constants.AUTH_DELETE_ACCOUNT: {
+          updateExpression = "SET accountDeletedOn = :accountDeletedOn, userEmail = :userEmail, nameParts = :nameParts, clientName = :clientName,  redirectUri = :redirectUri";
+          expressionAttributeValues = {
+            ":accountDeletedOn": returnRecord.accountDeletedOn,
+            ":userEmail": returnRecord.userEmail,
+            ":nameParts": returnRecord.nameParts,
+            ":clientName": returnRecord.clientName,
+            ":redirectUri": returnRecord.redirectUri,
+          };
+          break;
+        }
+        default:
+          this.handleError(`Unexpected event received in SQS queue: ${eventName}`, MessageCodes.MISSING_MANDATORY_FIELDS);
+      }
+
+      if (!updateExpression || !expressionAttributeValues) {
+        this.handleError(`Missing config to update DynamoDB for event: ${eventName}`, MessageCodes.MISSING_MANDATORY_FIELDS);
+      } else {
+				const saveEventData = await this.iprService.saveEventData(userId, updateExpression, expressionAttributeValues);
+
+				return {
+					statusCode: HttpCodesEnum.CREATED,
+					eventBody: saveEventData ? saveEventData : "OK",
 				}
-				case Constants.F2F_YOTI_START: {
-					updateExpression = "SET journeyWentAsyncOn = :journeyWentAsyncOn, expiresOn = :expiresOn";
-					expressionAttributeValues = {
-						":journeyWentAsyncOn": returnRecord.journeyWentAsyncOn,
-						":expiresOn": returnRecord.expiresDate,
-					};
-
-					if (returnRecord.postOfficeInfo) {
-						updateExpression += ", postOfficeInfo = :postOfficeInfo";
-						expressionAttributeValues[":postOfficeInfo"] = returnRecord.postOfficeInfo;
-					} else {
-						this.logger.info(`No post_office_details in ${eventName} event`);
-					}
-
-					if (returnRecord.documentType) {
-						updateExpression += ", documentType = :documentType";
-						expressionAttributeValues[":documentType"] = returnRecord.documentType;
-					} else {
-						this.logger.info(`No document_details in ${eventName} event`);
-					}
-					
-					if (returnRecord.clientSessionId) {
-						updateExpression += ", clientSessionId = :clientSessionId";
-						expressionAttributeValues[":clientSessionId"] = returnRecord.clientSessionId;
-					} else {
-						this.logger.info(`No govuk_signin_journey_id in ${eventName} event`);
-					}
-					break;
-				}
-				case Constants.IPV_F2F_CRI_VC_CONSUMED: {
-					if (!eventDetails.restricted || !eventDetails.restricted.nameParts) {
-						this.logger.error( { message: "Missing nameParts fields required for IPV_F2F_CRI_VC_CONSUMED event type" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-						throw new AppError(HttpCodesEnum.SERVER_ERROR, `Missing info in sqs ${Constants.AUTH_IPV_AUTHORISATION_REQUESTED} event`);
-					}
-					updateExpression = "SET readyToResumeOn = :readyToResumeOn, nameParts = :nameParts";
-					expressionAttributeValues = {
-						":readyToResumeOn": returnRecord.readyToResumeOn,
-						":nameParts": returnRecord.nameParts,
-					};
-
-					if (returnRecord.documentExpiryDate) {
-						updateExpression += ", documentExpiryDate = :documentExpiryDate";
-						expressionAttributeValues[":documentExpiryDate"] = returnRecord.documentExpiryDate;
-					} else {
-						this.logger.info(`No docExpiryDate in ${eventName} event`);
-					}
-					break;
-				}
-				case Constants.F2F_DOCUMENT_UPLOADED: {
-					if (!eventDetails.extensions || !eventDetails.extensions.post_office_visit_details) {
-						this.logger.error( { message: "Missing post_office_visit_details fields required for F2F_DOCUMENT_UPLOADED event type" }, { messageCode: MessageCodes.MISSING_MANDATORY_FIELDS });
-						throw new AppError(HttpCodesEnum.SERVER_ERROR, `Missing info in sqs ${Constants.F2F_DOCUMENT_UPLOADED} event`);
-					}
-					updateExpression = "SET documentUploadedOn = :documentUploadedOn, postOfficeVisitDetails = :postOfficeVisitDetails";
-					expressionAttributeValues = {
-						":documentUploadedOn": returnRecord.documentUploadedOn,
-						":postOfficeVisitDetails": returnRecord.postOfficeVisitDetails,
-					};
-					break;
-				}
-				case Constants.AUTH_DELETE_ACCOUNT: {
-					updateExpression = "SET accountDeletedOn = :accountDeletedOn, userEmail = :userEmail, nameParts = :nameParts, clientName = :clientName,  redirectUri = :redirectUri";
-					expressionAttributeValues = {
-						":accountDeletedOn": returnRecord.accountDeletedOn,
-						":userEmail": returnRecord.userEmail,
-						":nameParts":returnRecord.nameParts,
-						":clientName": returnRecord.clientName,
-						":redirectUri": returnRecord.redirectUri,
-					};
-					break;
-				}
-				default:
-					this.logger.error({ message: "Unexpected event received in SQS queue:", eventName });
-					throw new AppError(HttpCodesEnum.SERVER_ERROR, "Unexpected event received");
 			}
+    } catch (error: any) {
+      this.handleError("Cannot parse event data", MessageCodes.MISSING_MANDATORY_FIELDS);
+    }
+  }
 
-			if (!updateExpression || !expressionAttributeValues) {
-				this.logger.error({ message: "Missing config to update DynamboDB for event:", eventName });
-				throw new AppError(HttpCodesEnum.SERVER_ERROR, "Missing event config");
-			}
+  private handleError(errorMessage: string, messageCode: string): void {
+    this.logger.error({ message: errorMessage }, { messageCode: messageCode });
+    throw new AppError(HttpCodesEnum.BAD_REQUEST, errorMessage);
+  }
 
-			const saveEventData = await this.iprService.saveEventData(userId, updateExpression, expressionAttributeValues);
-
-			return {
-				statusCode: HttpCodesEnum.CREATED,
-				eventBody: saveEventData ? saveEventData : "OK",
-			};
-
-		} catch (error: any) {
-			this.logger.error({ message: "Cannot parse event data", error });
-			throw new AppError(HttpCodesEnum.BAD_REQUEST, "Cannot parse event data");
-		}
-	}
-
-	/**
-	 * Checks if all string values in the array are defined and does not
-	 * contain spaces only
-	 *
-	 * @param params
-	 */
-	checkIfValidString(params: Array<string | undefined>): boolean {
-		if (params.some((param) => (!param || !param.trim()) )) {
-			return false;
-		}
-		return true;
-	}
+  private checkIfValidString(params: Array<string | undefined>): boolean {
+    return params.every((param) => param && param.trim() !== "");
+  }
 }
