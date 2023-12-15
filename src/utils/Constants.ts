17beced9f5cc8f6f2dbe874291f4e7be090b449e
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-12-15 11:36:21 +0000

    Add govuk_signin_journey_id to TXMA_FIELDS_TO_SHOW

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -45,5 +45,5 @@ export class Constants {
     static readonly VISIT_PO_EMAIL_FALLBACK = "VISIT_PO_EMAIL_FALLBACK";
     
 
-	static readonly TXMA_FIELDS_TO_SHOW = ["event_name", "documentType", "docExpiryDate"];
+	static readonly TXMA_FIELDS_TO_SHOW = ["event_name", "documentType", "docExpiryDate", "govuk_signin_journey_id"];
 }
