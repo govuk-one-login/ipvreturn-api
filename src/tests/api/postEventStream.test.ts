2403e2d648cb20d83e12f448640c95d7d37509cb
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-05-07 09:55:00 +0100

    KIWI-1756 - Add Test to check SESSION_EVENTS_TABLE after F2F_YOTI_START event

--- src/tests/api/postEventStream.test.ts
+++ src/tests/api/postEventStream.test.ts
@@ -19,7 +19,48 @@ describe("post event processor", () => {
 		console.log("userId: ", userId);
 	});
 
-	it("when all 3 events are sent, a Dynamo record with the details of all three events populated", async () => {
+	it("when AUTH_IPV_AUTHORISATION_REQUESTED and F2F_YOTI_START events are sent, a Dynamo record with the details of both events is recorded", async () => {
+		await postMockEvent(VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT, userId, true);
+		await postMockEvent(VALID_F2F_YOTI_START_WITH_PO_DOC_DETAILS_TXMA_EVENT, userId, false);
+		
+		const response = await getSessionByUserId(userId, constants.API_TEST_SESSION_EVENTS_TABLE!);
+
+		expect(response?.clientName).toBe("ekwU");
+		expect(response?.redirectUri).toBe("REDIRECT_URL");
+		expect(response?.userEmail).toBe(constants.API_TEST_EMAIL_ADDRESS);
+		expect(response?.documentType).toBe("PASSPORT");
+		expect(response?.postOfficeInfo).toEqual([
+			{
+				"M": {
+					"address": {
+						"S": "1 The Street, Funkytown",
+					},
+					"location": {
+						"L": [
+							{
+								"M": {
+									"latitude": {
+										"N": "0.34322",
+									},
+									"longitude": {
+										"N": "-42.48372",
+									},
+								},
+							},
+						],
+					},
+					"name": {
+						"S": "Post Office Name",
+					},
+					"post_code": {
+						"S": "N1 2AA",
+					},
+				},
+			},
+		]);
+	}, 20000); 
+
+	it("when all 4 events are sent, a Dynamo record with the details of all 4 events is recorded", async () => {
 		await postMockEvent(VALID_AUTH_IPV_AUTHORISATION_REQUESTED_TXMA_EVENT, userId, true);
 		await postMockEvent(VALID_F2F_YOTI_START_WITH_PO_DOC_DETAILS_TXMA_EVENT, userId, false);
 		await postMockEvent(VALID_F2F_DOCUMENT_UPLOADED_TXMA_EVENT, userId, false);
