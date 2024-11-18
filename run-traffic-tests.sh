9367e10b8f9f1607a1eddb2c0b35c4bb2dd391d8
abdul-gds <104900842+abdul-gds@users.noreply.github.com>
2024-11-18 10:49:35 +0000

    Create run-traffic-tests.sh

---
+++ run-traffic-tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+remove_quotes () {
+  echo "$1" | tr -d '"'
+}
+
+declare error_code
+# shellcheck disable=SC2154
+#The CFN variables seem to include quotes when used in tests these must be removed before assigning them.
+export API_TEST_SESSION_EVENTS_TABLE=$(remove_quotes $CFN_SessionEventsTable)
+export GOV_NOTIFY_API=$(remove_quotes $CFN_GovNotifyAPIURL)
+export DEV_IPR_TEST_HARNESS_URL=$(remove_quotes $CFN_IpvReturnTestHarnessURL)
+
+# disabling error_check to allow report generation for successful + failed tests
+set +e
+cd /src; npm run test:api 
+error_code=$?
+cp -rf results $TEST_REPORT_ABSOLUTE_DIR
+if [ $error_code -ne 0 ]
+then
+  exit $error_code
+fi
