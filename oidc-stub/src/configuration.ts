445d93579e9a505f763bc913b0bf0f634693007e
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2026-03-18 16:15:37 +0000

    KIWI-1888: POC branch that utilises oidc-node-provider as a stub in place of calling auth

---
+++ oidc-stub/src/configuration.ts
@@ -0,0 +1,52 @@
+import { Configuration } from 'oidc-provider';
+import Account from './account.js';
+
+const configuration: Configuration = {
+  findAccount: Account.findAccount,
+    clients: [
+    {
+      client_id: process.env.OIDC_CLIENT_ID || "123456789",
+      redirect_uris: [process.env.REDIRECT_URI || "https://localhost:8080"],
+      response_types: ["code"],
+      grant_types: ["authorization_code"],
+      token_endpoint_auth_method: "private_key_jwt",
+      jwks: {
+        keys: [
+          //JWK value can be set for testing should be using jwks endpoint
+        ]
+      }    
+    },
+  ],
+  interactions: {
+    url(ctx, interaction) {
+      return `/interaction/${interaction.uid}`;
+    },
+  },
+  cookies: {
+    keys: ['some secret key'],
+    short: {
+      httpOnly: true,
+      overwrite: true,
+      sameSite: 'none',
+      secure: true,
+    },
+  },
+  claims: {
+    address: ['address'],
+    email: ['email', 'email_verified'],
+    phone: ['phone_number', 'phone_number_verified'],
+    profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name', 'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo'],
+  },
+  features: {
+    devInteractions: { enabled: false }, // Using custom routes instead
+  },
+  pkce: {
+    required:() => false
+  },
+  routes: {
+    authorization: "/authorize",
+    token: '/token',
+  }
+};
+
+export default configuration;
--- oidc-stub/src/utils/Constants.ts
+++
@@ -1,5 +0,0 @@
-export class Constants {
-		static readonly AUTHORIZATIONCODE_LOGGER_SVC_NAME = "AuthorizationCodeHandler";
-		static readonly IPR_METRICS_NAMESPACE = "IPR-CRI";
-		static readonly DEBUG = "DEBUG";
-	}
