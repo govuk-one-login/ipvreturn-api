08ea9146dfa88cd51bec3b305d44125d6b518526
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-05-21 16:47:58 +0100

    Sonar coverage

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       enabled: true,
       provider: "v8",
       reportsDirectory: "coverage",
+      reporter: ["lcov", "text"],
     },
     reporters: ["default"],
   },
