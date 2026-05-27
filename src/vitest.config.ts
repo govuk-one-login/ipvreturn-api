91b4c9dcb500a05ef792d6e37cab602774c02e69
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-05-27 15:05:48 +0100

    Reverted

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -10,7 +10,17 @@ export default defineConfig({
     coverage: {
       enabled: true,
       provider: "v8",
+      include: ["**/*.ts"],
+      exclude: [
+        "**/tests/**/*.ts",
+        "**/types/**/*.ts",
+        "vitest.config.ts",
+        "vitest.setup.ts",
+        "**/node_modules/**",
+        "tsconfig.json",
+      ],
       reportsDirectory: "coverage",
+      reporter: ["lcov", "text"],
     },
     reporters: ["default"],
   },
