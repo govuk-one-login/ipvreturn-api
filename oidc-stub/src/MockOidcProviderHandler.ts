2c258e880ba10599752f218a935e270fb147e75c
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2026-05-29 14:30:33 +0100

    WIP

--- oidc-stub/src/MockOidcProviderHandler.ts
+++ oidc-stub/src/MockOidcProviderHandler.ts
@@ -4,14 +4,11 @@ import serverlessExpress from '@codegenie/serverless-express';
 import configuration from './configuration';
 import routes from './routes';
 import * as path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import ejs from 'ejs';
 
 const app = express();
 
 // Basic middleware
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('ejs', ejs.renderFile);
