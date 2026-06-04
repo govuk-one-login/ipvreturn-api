a46506022c7e6268d8e4a84ba776321efcf8dc03
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-06-04 14:12:09 +0100

    vite loadEnv imported by vitest.config to allow use of .env file for local testing

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from 'vite'
 
 export default defineConfig({
   test: {
+    env: loadEnv('', process.cwd(), ''),
     globals: true,
     environment: "node",
     setupFiles: ["./vitest.setup.ts"],
